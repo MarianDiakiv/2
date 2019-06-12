f5afce6041300c499fbd4a257615a13164c7920f
MarianDiakiv <mariandiakiv@gmail.com>
2019-06-12 18:55:17 +0300

    add user

--- src/com/company/TestGit/Person.java
+++ src/com/company/TestGit/Person.java
@@ -3,4 +3,5 @@ package com.company.TestGit;
 public class Person {
     public int age;
 public  String cn;
+public  double price;
 }
---
+++ src/com/company/TestGit/USer.java
@@ -0,0 +1,6 @@
+package com.company.TestGit;
+
+public class USer {
+    public   String username;
+    public  String pass;
+}
